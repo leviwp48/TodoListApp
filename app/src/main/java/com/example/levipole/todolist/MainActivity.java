dd1cfe173d21bdb50b47d33d9567ceacdcc9f036
leviwp48 <leviwp48@gmail.com>
2018-10-07 22:36:59 -0700

    Working
    Finished adding, deleting, file persistence

--- app/src/main/java/com/example/levipole/todolist/MainActivity.java
+++ app/src/main/java/com/example/levipole/todolist/MainActivity.java
@@ -2,12 +2,107 @@ package com.example.levipole.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // a numberic code to identify the edit activity
+    public final static int EDIT_REQUEST_CODE = 20;
+    // keys used for passing data between activities
+    public final static String ITEM_TEXT = "itemText";
+    public final static String ITEM_POSITION = "itemPosition";
+
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items); //needs 3 arguments. this = this activity, type of item, items = list of items;
+        lvItems = (ListView) findViewById(R.id.lvItems); //R class compiled at run time, not the same as above android.R
+        lvItems.setAdapter(itemsAdapter);
+
+        // mock data
+        // items.add("First item");
+        // items.add("Second item");
+
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v){
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+        Toast.makeText(getApplicationContext(),"Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+    // private since we are calling directly not acking framework to do it
+    // Keep in mind only setup of listener is done at runtime. Content of listener doens't happen until event
+    private void setupListViewListener(){
+        // Can use logging to check when code is executed. Logs sorted by priority (7 Highest, 2 Loweset)
+        Log.i("MainActivity", "Setting up Listener on list view");
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+               // Here position of item and underlying Array of items matches up
+               // Other cases might need to convert position
+                Log.i("MainActivity", "Item removed from list: " + position);
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        // set up item listener for edit (regular click)
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // create the new activity
+            }
+        });
+    }
+
+    private File getDataFile(){
+        // Using android storage stystem
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading file", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e){
+            Log.e("MainActivity", "Error writing file", e);
+        }
     }
 }
